627a2ec228950caa2e5921a0eb7ca0de3587913d
Sondre Mære Overskaug <sondremo@gmail.com>
2016-10-14 15:02:05 +0200

    feature(android): add support for badge count
    Android does not natively support badge counters, so we utilize shortcut badger to be able to show badge counters.
    To trigger these updates one needs to send a "badge" parameter in the "data" payload of a push notification. If the app is dead (as opposed to just in the background, one cannot send the "notification" payload in the push notification as the OS then takes over control.

--- android/src/main/java/com/evollu/react/fcm/MessagingService.java
+++ android/src/main/java/com/evollu/react/fcm/MessagingService.java
@@ -1,7 +1,9 @@
 package com.evollu.react.fcm;
 
+import java.util.Map;
 import android.content.Intent;
 import android.util.Log;
+import me.leolin.shortcutbadger.ShortcutBadger;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -16,6 +18,19 @@ public class MessagingService extends FirebaseMessagingService {
         Log.d(TAG, "Remote message received");
         Intent i = new Intent("com.evollu.react.fcm.ReceiveNotification");
         i.putExtra("data", remoteMessage);
+        if(remoteMessage.getData() != null){
+            Map data = remoteMessage.getData();
+            if (data.get("badge") != null) {
+                int badgeCount = Integer.parseInt((String)data.get("badge"));
+                if (badgeCount == 0) {
+                    Log.d(TAG, "Remove count");
+                    ShortcutBadger.removeCount(this);
+                } else {
+                    Log.d(TAG, "Apply count: " + badgeCount);
+                    ShortcutBadger.applyCount(this, badgeCount);
+                }
+            }
+        }
         sendOrderedBroadcast(i, null);
     }
 }
